8ae46cc00f93ba04f756122f751d8437d6e17e61
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2020-09-21 18:39:58 -0700

    final

--- storyboard.py
+++ storyboard.py
@@ -4,37 +4,65 @@ print("")
 print("Welcome to the Oregon Trail 2.0")
 path = input("Take the left of right path:  ")
 
-if path == "left":
-    print("Your wagon broke! Result: death")
-    print("")
-    print("------------------------")
-    print("")
-elif path == "right":
+if path == "right":
     print("Congrats! You lived.")
     print("")
     print("------------------------")
     print("")
     food = input("Eat berries or rations:  ")
-    if food == "berries":
-        print("The berries were poisoned. Result: death")
-        print("")
-        print("------------------------")
-        print("")
-    elif food == "rations":
+
+    if food == "rations":
         print("Your rations have restored your health!")
         print("")
         print("------------------------")
         print("")
         shelter = input("Take shelter in cave or city:  ")
-        if shelter == "city":
-            print("You got attacked by raiders. Result: death")
-            print("")
-            print("------------------------")
-            print("")
-        elif shelter == "cave":
+    
+        if shelter == "cave":
             print("You have survived the night!")
             print("")
             print("------------------------")
             print("")
+            river = input("There's a river up ahead. Swim or use a boat:  ")
+
+            if river == "boat":
+                print("You have evaded the crocodiles!")
+                print("")
+                print("------------------------")
+                print("")
+                water = input("Filter your water before drinking?")
+
+                if water == "yes":
+                    print("Congrats! You have made it to Oregon!")
+                    print("")
+                    print("------------------------")
+                    print("")
+                    
+                else:
+                    print("You have died of dysentary.")
+                    print("")
+                    print("------------------------")
+                    print("")
+            else:
+                print("You got eaten by crocodiles. Result: death")
+                print("")
+                print("------------------------")
+                print("")
+
+        else:
+            print("You got attacked by raiders. Result: death")
+            print("")
+            print("------------------------")
+            print("")
+
+    else:
+        print("The berries were poisoned. Result: death")
+        print("")
+        print("------------------------")
+        print("")
+        
 else:
-    print("You died.")
+    print("Your wagon broke! Result: death")
+    print("")
+    print("------------------------")
+    print("")
