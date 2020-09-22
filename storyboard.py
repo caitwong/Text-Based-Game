c3a04ce8555e41e60e8bc23fadf40ca1b442275e
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2020-09-21 18:12:36 -0700

    more

--- storyboard.py
+++ storyboard.py
@@ -1,4 +1,4 @@
-print("Welcome to the Oregon Trail")
+print("Welcome to the Oregon Trail 2.0")
 path = input("Take the left of right path:  ")
 
 if path == "left":
@@ -10,6 +10,10 @@ elif path == "right":
         print("The berries were poisoned. Result: death")
     elif food == "rations":
         print("Your rations have restored your health!")
-
+        shelter = input("Take shelter in cave or city:  ")
+        if shelter == "city":
+            print("You got attacked by raiders. Result: death")
+        elif shelter == "cave":
+            print("You have survived the night!")
 else:
     print("You died.")
