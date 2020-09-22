3ed4fb947b696bf26bbe5938bc0d4918041b461c
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2020-09-21 18:00:46 -0700

    second

---
+++ storyboard.py
@@ -0,0 +1,8 @@
+print("Welcome to Build Your own Icecream")
+choco = input("Do you want Chocolate?")
+
+if choco = "yes":
+    print("Do you want sprinkles with that?")
+elif choco = "no":
+    print("")
+    
