66fdc8d8eba59a9a63bf31c7d20b80272de0b712
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2020-09-21 18:09:17 -0700

    updates

--- storyboard.py
+++ storyboard.py
@@ -1,8 +1,15 @@
-print("Welcome to Build Your own Icecream")
-choco = input("Do you want Chocolate?")
+print("Welcome to the Oregon Trail")
+path = input("Take the left of right path:  ")
 
-if choco = "yes":
-    print("Do you want sprinkles with that?")
-elif choco = "no":
-    print("")
-    
+if path == "left":
+    print("Your wagon broke! Result: death")
+elif path == "right":
+    print("Congrats! You lived.")
+    food = input("Eat berries or rations:  ")
+    if food == "berries":
+        print("The berries were poisoned. Result: death")
+    elif food == "rations":
+        print("Your rations have restored your health!")
+
+else:
+    print("You died.")
